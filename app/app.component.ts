7eeb6c9f79c9e8f8d8d92cb6ff1604dffd5830df
WDRed <one.red.wlad@gmail.com>
2016-10-18 16:08:34 +0300

    changed package.json start option to start the app, part with Hero class doesn`t work

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My SECOND Angular App</h1>'
+    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
 })
-export class AppComponent { }
+
+export class Hero  {
+	
+	id: number;
+	name: string;
+
+}
+export class AppComponent { 
+
+	title = 'Tour of Heroes';
+	hero: Hero = {
+		id: 1,
+		name: 'Windstorm'
+	};
+}
