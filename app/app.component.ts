9d4cfe9d921354e5b0bece42b26831e07ee19ed4
WDRed <one.red.wlad@gmail.com>
2016-10-18 16:24:34 +0300

    resolved issue with Hero class - variable must be defined before it is used

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 
-@Component({
-    selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
-})
-
 export class Hero  {
 	
 	id: number;
 	name: string;
 
 }
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
+})
+
 export class AppComponent { 
 
 	title = 'Tour of Heroes';
