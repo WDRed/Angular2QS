e3b6b958f00f31b3d2c8b9c4d166ae7c2b63e7d4
WDRed <one.red.wlad@gmail.com>
2016-10-18 16:38:30 +0300

    used two way data binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,7 +9,16 @@ export class Hero  {
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
+    template: `
+    	<h1>{{title}}</h1>
+    	<h2>{{hero.name}} details!</h2>
+    	<div><label>id: </label>{{hero.id}}</div>
+    	<div>
+    		<label>name: </label>
+    		<input [(ngModel)]="hero.name" placeholder="name">
+    	</div>
+
+    `
 })
 
 export class AppComponent { 
