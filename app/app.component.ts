bfe1543c220bb2904e3b9eef2bc4e451aa158f05
WDRed <one.red.wlad@gmail.com>
2016-10-20 15:23:37 +0300

    add hero.service.ts, mock-heroes.ts, created providers for HeroService

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,9 @@
 import { Component } from '@angular/core';
 
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
-
-const HEROES: Hero[] =[
-	{ id: 11, name: 'Mr. Nice' },
-	{ id: 12, name: 'Narco' },
-	{ id: 13, name: 'Bombasto' },
-	{ id: 14, name: 'Celeritas' },
-	{ id: 15, name: 'Magneta' },
-	{ id: 16, name: 'RubberMan' },
-	{ id: 17, name: 'Dynama' },
-	{ id: 18, name: 'Dr IQ' },
-	{ id: 19, name: 'Magma' },
-	{ id: 20, name: 'Tomato' }
-];
+import { OnInit } from '@angular/core';
 
 @Component({
     selector: 'my-app',
@@ -78,16 +67,25 @@ const HEROES: Hero[] =[
 			margin-right: .8em;
 			border-radius: 4px 0 0 4px;
 		}
-    `]
+    `],
+    providers: [HeroService]
 })
 
-export class AppComponent { 
+export class AppComponent implements OnInit { 
 
 	title = 'Tour of Heroes';
 	selectedHero: Hero;
-	heroes = HEROES;
+	heroes: Hero[];
+
+	constructor(private heroService: HeroService){};
 
 	onSelect(hero:Hero): void{
 		this.selectedHero = hero;
+	}
+	getHeroes(): void{
+		this.heroes = this.heroService.getHeroes();
+	}
+	ngOnInit(): void {
+		this.getHeroes();
 	}
 }
